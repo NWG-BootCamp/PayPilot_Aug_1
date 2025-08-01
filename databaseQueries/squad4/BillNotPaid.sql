9a3c34e8558a21e633d987c822f8d039a3e66919
jahnavimurali <jahnavim3103@gmail.com>
2025-08-01 15:10:35 +0530

    delete squad 4 dir

--- databaseQueries/squad4/BillNotPaid.sql
+++
@@ -1,7 +0,0 @@
-SELECT name, email
-FROM users
-WHERE user_id NOT IN (
-    SELECT DISTINCT user_id
-    FROM bills
-    WHERE is_paid = TRUE
-);
--- databaseQueries/squad4/giveReminders.sql
+++
@@ -1,4 +0,0 @@
-SELECT rs.user_id
-FROM reminder_settings rs
-JOIN tasks t ON rs.user_id = t.user_id
-WHERE t.due_date = CURRENT_DATE + rs.days_before_due;
