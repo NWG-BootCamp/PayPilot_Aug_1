21a97fe1d9b0ff6f47cd39b92e0275ddb64bd7bd
Yajat Gupta <128251482+GuptaYajat@users.noreply.github.com>
2025-08-01 14:58:02 +0530

    Update ReminderSettings.sql

--- databaseQueries/squad4/ReminderSettings.sql
+++ databaseQueries/squad4/ReminderSettings.sql
@@ -10,3 +10,28 @@ CREATE TABLE ReminderSettings (
     FOREIGN KEY (bill_id) REFERENCES Bills(bill_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
+
+
+-- Sample reminder_settings table
+-- reminder_id | user_id | days_before_due
+
+INSERT INTO reminder_settings (reminder_id, user_id, days_before_due) VALUES
+(1, 101, 3),
+(2, 102, 5),
+(3, 103, 2);
+
+
+-- Sample tasks table: user_id | due_date
+
+INSERT INTO tasks (user_id, due_date) VALUES
+(101, CURRENT_DATE + INTERVAL '3 day'),  -- should be reminded today
+(102, CURRENT_DATE + INTERVAL '5 day'),  -- should be reminded today
+(103, CURRENT_DATE + INTERVAL '1 day');  -- NOT reminded today
+
+
+-- for giving reminders
+
+SELECT rs.user_id
+FROM reminder_settings rs
+JOIN tasks t ON rs.user_id = t.user_id
+WHERE t.due_date = CURRENT_DATE + rs.days_before_due;
